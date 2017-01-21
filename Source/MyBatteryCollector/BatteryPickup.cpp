b5a633c7edd1ea5f69819a85fe39412be369a59c
Denis Dubinin <dadubinin@gmail.com>
2017-01-21 17:21:16 +0000

    extend pickup

---
+++ Source/MyBatteryCollector/BatteryPickup.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyBatteryCollector.h"
+#include "BatteryPickup.h"
+
+/* set default values */
+ABatteryPickup::ABatteryPickup()
+{
+	GetMesh()->SetSimulatePhysics(true);
+}
+
+
